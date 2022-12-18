c8e740546cdddfccd98581995d7791b5fd5c1607
nikkivasilev <n.vasilev105@gmail.com>
2022-12-18 15:29:50 +0200

    fixed tests

--- magazinslunce/tests/accounts/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .user_details_view_tests import *
--- tests/validators/file_size_validator_tests.py
+++ tests/validators/file_size_validator_tests.py
@@ -3,17 +3,29 @@ from unittest import TestCase
 from magazinslunce.common.validators import validate_file_size
 
 
+class Objwithsize:
+    def __init__(self, size):
+        self.size = size
+
+
 class FileSizeValidator(TestCase):
     SIZE_LIMIT = 5 * 1024 * 1024
 
     def test_file_size_validator_when_size_correct(self):
-        validate_file_size(self.SIZE_LIMIT - 20)
+        obj = Objwithsize
+        obj.size = self.SIZE_LIMIT - 20
+        validate_file_size(obj)
 
     def test_file_size_validator_when_size_exact_limit(self):
-        validate_file_size(self.SIZE_LIMIT)
+        obj = Objwithsize
+        obj.size = self.SIZE_LIMIT
+        validate_file_size(obj)
 
     def test_file_size_validator_when_size_incorrect(self):
+        obj = Objwithsize
+        obj.size = self.SIZE_LIMIT + 20
+
         with self.assertRaises(Exception) as context:
-            validate_file_size(self.SIZE_LIMIT + 20)
+            validate_file_size(obj.size)
 
         self.assertIsNotNone(context.exception)
